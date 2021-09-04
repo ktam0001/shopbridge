c68de0ed7d96e6c0f75d6af63ee76c2faa606373
KTam <karantamrakar8@gmail.com>
2021-09-04 17:30:04 +0530

    Deletion functionalit added

--- src/components/GetProducts.js
+++ src/components/GetProducts.js
@@ -1,26 +1,59 @@
-import React from 'react'
+import React, {useState} from 'react'
 import inventoryDB from './data/inventoryDB';
+import './styles/getproducts.css';
 
 function GetProducts(){
-            return inventoryDB.map((product,index)=>{
-                return (
-                    <div key = {index} class="ui card">
-                            <div class="image">
-                                <img src={product.product_img}/>
-                            </div>
-                            <div class="content">
-                                <p class="header">{product.product_name}</p>
-                                <div class="meta">
-                                    <p>Price : {product.product_price}</p>
-                                </div>
-                                <div class="description">
-                                    <p>Description : </p>
-                                    {product.product_description}
-                                </div>
-                            </div>
+
+    const [inventoryData, setInventoryData] = useState(inventoryDB);
+
+    const onDelete = (index) => {
+        // inventoryDB.splice(index,1);
+        let temp = [...inventoryData];
+        temp.splice(index,1)
+        setInventoryData(temp);
+        console.log(inventoryData);       
+    }
+
+    return inventoryData.map((product,index)=>{
+        return (
+            <div key = {index} className = "ui card">
+                    <div className ="image">
+                        <img className = "getproduct-img" src={product.product_img}/>
                     </div>
-                );        
-            });
+                    <div className = "content">
+                        <p className = "header">{product.product_name}</p>
+                        <div className = "meta">
+                            <p>Price : {product.product_price}</p>
+                        </div>
+                        <div className = "description">
+                            <p>Description : </p>
+                            {product.product_description}
+                        </div>
+                        <button className = 'getproduct-delete' type = 'submit' onClick = {() => onDelete(index)}>delete</button>
+                    </div>
+            </div>
+        );        
+    });
+            // return inventoryDB.map((product,index)=>{
+            //     return (
+            //         <div key = {index} className = "ui card">
+            //                 <div className ="image">
+            //                     <img className = "getproduct-img" src={product.product_img}/>
+            //                 </div>
+            //                 <div className = "content">
+            //                     <p className = "header">{product.product_name}</p>
+            //                     <div className = "meta">
+            //                         <p>Price : {product.product_price}</p>
+            //                     </div>
+            //                     <div className = "description">
+            //                         <p>Description : </p>
+            //                         {product.product_description}
+            //                     </div>
+            //                     <button className = 'getproduct-delete' type = 'submit' onClick = {() => onDelete(index)}>delete</button>
+            //                 </div>
+            //         </div>
+            //     );        
+            // });
 }
 
 export default GetProducts;
